632faa4abb506cef59530e80e280f0a325016635
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2020-03-06 16:56:51 -0500

    updated

--- src/actions/BotsReplyAction.js
+++ src/actions/BotsReplyAction.js
@@ -1,32 +1,11 @@
-import { React } from 'react';
-import propTypes from 'prop-types';
+import { request } from '../services/request';
 
-
-export const getBotsReply = () => {
-  return fetch('http://localhost:5000/adMed/botConvo/botReply')
-  .then(res => ([res.ok, res.json()]))
-  .then(([ok, json]) => {
-    if(!ok) throw 'Unable to fetch the bots reply';
-    
-    return json;
-  })
-  //.then structure the json file to have the sessionID and the bots reply 
-}
-
-export function FetchBotsReply() {
-  //eventually we will dispatch the results into the redux store to save with return function(dispatch) and wrap the code within that 
-  return getBotsReply()
-  .then(res => console.log(res.text))
-}
-
-export function PostBotsReply(botsReply) {
-  return (
-    <>
-    <p>{botReply}</p>
-    </>
-  )
-}
-
-PostBotsReply.propTypes = {
-  botsReply: propTypes.string.isRequired
+export function getBotsReply() {
+    return request()
+    .then(res => {
+        return res
+    })
+    .then(res => {
+        console.log("RES", res)
+    })
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { FetchBotsReply, PostBotReply } from '../actions/BotsReplyAction';
+import { getBotsReply, PostBotReply } from '../actions/BotsReplyAction';
 
 export default function App() {
   //bot reply component here
   //probably dont need a fn here just the jsax
-  return FetchBotsReply();
+  return getBotsReply();
 }
