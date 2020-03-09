b6a152e381bbca4ba6d0ea26814a24805e817dca
Jose Lora <jose.lora@aponia.co>
2020-03-09 09:27:41 -0400

    some changes in conversation and frontend styles

--- src/actions/BotsReplyAction.js
+++ src/actions/BotsReplyAction.js
@@ -1,11 +1,41 @@
-import { request } from '../services/request';
+// import React, { useState, useEffect } from 'react';
+// import propTypes from 'prop-types';
 
-export function getBotsReply() {
-    return request()
-    .then(res => {
-        return res
-    })
-    .then(res => {
-        console.log("RES", res)
-    })
-}
+export default async params => {
+
+    const data = {userMessage: 'what are the side effects of lipitor'};
+
+    const result = await fetch('http://localhost:5000/adMed/botConvo/botReply',
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify(data)
+      }).then(res => {
+        if(res.status >= 200 && res.status <= 299){
+          return res.json();
+        }else{
+          console.log(res.statusText);
+        }
+      }).then((json) => {
+        console.log(json)
+        return json;
+        // setConversation(conversation => [...conversation, json]);
+      });
+  //eventually we will dispatch the results into the redux store to save with return function(dispatch) and wrap the code within that 
+      return result;
+};
+
+// export function PostBotsReply(botsReply) {
+//   return (
+//     <>
+//     <p>{botReply}</p>
+//     </>
+//   )
+// }
+
+// PostBotsReply.propTypes = {
+//   botsReply: propTypes.string.isRequired
+// }
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,54 @@
-import React from 'react';
-import { getBotsReply, PostBotReply } from '../actions/BotsReplyAction';
+import React, {useEffect, useState} from 'react';
+import {Container, Row, Form, Col, Button} from 'react-bootstrap';
+import fetchBotsReply from '../actions/BotsReplyAction';
 
-export default function App() {
+function App() {
   //bot reply component here
   //probably dont need a fn here just the jsax
-  return getBotsReply();
+    
+  const [conversation, setConversation] = useState([]);
+
+  const chatBot = async data => {
+
+    var result = await fetchBotsReply({userMsg: "Hello"});
+    console.log(result)
+    setConversation(conversation => [...conversation, result]);
+
+  }
+
+  useEffect(() => {
+    chatBot("Hello")
+  }, [])
+
+  return (
+    <main>
+      <Container>
+          <Form.Group >
+            {conversation ? console.log(conversation): ''}
+            {Object.keys(conversation).map((item, i) => (
+              <Row key={i}>
+                {console.log(item)}
+                <Form.Label column md={2} >User</Form.Label>
+                <Form.Label column md={10} >{conversation[item].text}</Form.Label>
+              </Row>
+            ))
+            }
+          </Form.Group>
+      </Container>
+      <Container className="fixed-bottom">
+        <Form.Group as={Row}>
+          <Col md={10}>
+            <Form.Control type="text" placeholder="Message..." />
+          </Col>
+          <Col md={2}>
+            <Button block variant="primary" type="submit">
+              Send
+            </Button>
+          </Col>
+        </Form.Group>
+      </Container>
+    </main>
+  );
 }
+
+export default App;
