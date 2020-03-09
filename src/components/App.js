174515d1b5864fae99578692de75f244411826a8
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2020-03-09 17:31:24 -0400

    moved code from app to its own component displayConvo

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,7 @@
-import React, {useEffect, useState} from 'react';
-import {Container, Row, Form, Col, Button} from 'react-bootstrap';
-import fetchBotsReply from '../actions/BotsReplyAction';
+import displayConvo from './displayConvo';
 
 function App() {
-    
-  const [conversation, setConversation] = useState([]);
-
-  const chatBot = async data => {
-
-    var result = await fetchBotsReply({userMsg: "what are the adverse effects of chatex"}); //shouldnt data just be 'data' here
-    console.log(result)
-    setConversation(conversation => [...conversation, result]);//where is conversation being originally set, how can it be passed if not exist
-
-  }
-
-  //when component mounts/updates, call the chatbot with Hello. Should just be for initial load?
-  useEffect(() => {
-    chatBot("Hello") 
-  }, [])
-
-  return (
-    <main>
-      <Container>
-          <Form.Group >
-            {conversation ? console.log("conversation", conversation): ''}
-            {Object.keys(conversation).map((item, i) => (
-              <Row key={i}>
-                {console.log(item)}
-                <Form.Label column md={2} >User</Form.Label>
-                <Form.Label column md={10} >{conversation[item].text}</Form.Label>
-              </Row>
-            ))
-            }
-          </Form.Group>
-      </Container>
-      <Container className="fixed-bottom">
-        <Form.Group as={Row}>
-          <Col md={10}>
-            <Form.Control type="text" placeholder="Message..." />
-          </Col>
-          <Col md={2}>
-            <Button block variant="primary" type="submit">
-              Send
-            </Button>
-          </Col>
-        </Form.Group>
-      </Container>
-    </main>
-  );
+  return displayConvo()
 }
 
 export default App;
